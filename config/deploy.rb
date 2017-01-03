1fb96356dece73abf96f781e6888a14187dca027
Robert Houser <bobac@bobac.net>
2017-01-03 14:33:45 +0100

    init

---
+++ config/deploy.rb
@@ -0,0 +1,20 @@
+set :application, 'rails4test'
+set :repo_url, 'git@github.com:bobac/rails4test.git'
+
+set :deploy_to, '/home/deploy/rails4test'
+
+set :linked_files, %w{config/database.yml}
+set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+
+namespace :deploy do
+
+  desc 'Restart application'
+  task :restart do
+    on roles(:app), in: :sequence, wait: 5 do
+      execute :touch, release_path.join('tmp/restart.txt')
+    end
+  end
+
+  after :publishing, 'deploy:restart'
+  after :finishing, 'deploy:cleanup'
+end
