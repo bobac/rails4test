2ced79a6c0e2e6fb7fe28878a649ace21b4775c3
Robert Houser <bobac@bobac.net>
2017-01-05 18:36:03 +0100

    added root

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,4 @@
 set :stage, :production
 
 # Replace 127.0.0.1 with your server's IP address!
-server '35.157.9.223', user: 'deploy', roles: %w{web app}
+server '35.156.42.124', user: 'deploy', roles: %w{web app}
