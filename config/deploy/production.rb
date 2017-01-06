edc993d20c0564f880e14da7befb80523fb0291a
Robert Houser <bobac@bobac.net>
2017-01-06 18:45:41 +0100

    ec2 ip production

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,4 @@
 set :stage, :production
 
 # Replace 127.0.0.1 with your server's IP address!
-server '35.156.42.124', user: 'deploy', roles: %w{web app}
+server '35.156.104.128', user: 'deploy', roles: %w{web app}
